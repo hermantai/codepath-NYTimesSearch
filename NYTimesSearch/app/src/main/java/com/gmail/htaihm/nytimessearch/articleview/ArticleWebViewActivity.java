4fd763b220ee148c6c957e3855befe9916170b1d
Herman Tai <htaihm@gmail.com>
2016-02-13 15:58:46 -0800

    Add link sharing in the ToolBar in ArticleWebView, hide ToolBar when scrolling down

--- NYTimesSearch/app/src/main/java/com/gmail/htaihm/nytimessearch/articleview/ArticleWebViewActivity.java
+++ NYTimesSearch/app/src/main/java/com/gmail/htaihm/nytimessearch/articleview/ArticleWebViewActivity.java
@@ -3,7 +3,12 @@ package com.gmail.htaihm.nytimessearch.articleview;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -15,6 +20,9 @@ import com.gmail.htaihm.nytimessearch.model.Article;
 
 public class ArticleWebViewActivity extends AppCompatActivity {
     private static final String INTENT_EXTRA_ARTICLE = "com.gmail.htaihm.nytimessearch.article";
+
+    private ShareActionProvider mShareActionProvider;
+    private Article mArticle;
 
     public static Intent newIntent(Context context, Article article) {
         Intent i = new Intent(context, ArticleWebViewActivity.class);
@@ -28,12 +36,15 @@ public class ArticleWebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_web_view);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         final ProgressBar pbLoadingProgressBar = (ProgressBar) findViewById(
                 R.id.pbLoadingProgressBar);
         pbLoadingProgressBar.setMax(100);  // WebChromeClient reports in range 0-100
 
-        Article article = getIntent().getParcelableExtra(INTENT_EXTRA_ARTICLE);
-        String url = article.getWebUrl();
+        mArticle = getIntent().getParcelableExtra(INTENT_EXTRA_ARTICLE);
+        String url = mArticle.getWebUrl();
 
         WebView webView = (WebView) findViewById(R.id.wvArticle);
         webView.setWebChromeClient(new WebChromeClient(){
@@ -57,4 +68,25 @@ public class ArticleWebViewActivity extends AppCompatActivity {
         webView.loadUrl(url);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_article_web_view, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        mShareActionProvider.setShareIntent(createShareIntent());
+
+        return true;
+    }
+
+    public Intent createShareIntent() {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_SUBJECT, mArticle.getHeadline());
+        i.putExtra(Intent.EXTRA_TEXT, mArticle.getWebUrl());
+        i.setType("text/plain");
+        return i;
+    }
 }
